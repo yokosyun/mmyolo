f979e5d899c8170023e00cc3408f3a7b9d948fed
yokosyun <yoko.syun.1408@gmail.com>
2023-12-18 16:54:06 +0900

    add missing import

--- mmyolo/models/layers/__init__.py
+++ mmyolo/models/layers/__init__.py
@@ -5,12 +5,12 @@ from .yolo_bricks import (BepC3StageBlock, BiFusion, CSPLayerWithTwoConv,
                           ELANBlock, ImplicitA, ImplicitM,
                           MaxPoolAndStrideConvBlock, PPYOLOEBasicBlock,
                           RepStageBlock, RepVGGBlock, SPPFBottleneck,
-                          SPPFCSPBlock, TinyDownSampleBlock)
+                          SPPFCSPBlock, TinyDownSampleBlock,GroupFusedMBConv)
 
 __all__ = [
     'SPPFBottleneck', 'RepVGGBlock', 'RepStageBlock', 'ExpMomentumEMA',
     'ELANBlock', 'MaxPoolAndStrideConvBlock', 'SPPFCSPBlock',
     'PPYOLOEBasicBlock', 'EffectiveSELayer', 'TinyDownSampleBlock',
     'EELANBlock', 'ImplicitA', 'ImplicitM', 'BepC3StageBlock',
-    'CSPLayerWithTwoConv', 'DarknetBottleneck', 'BiFusion'
+    'CSPLayerWithTwoConv', 'DarknetBottleneck', 'BiFusion',"GroupFusedMBConv",
 ]
